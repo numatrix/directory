34ba04f129a0238acc0f7ddbaf0c4857d9af7dc3
Paramesh Gunasekaran <paramesh.gunasekaran@gmail.com>
2019-06-05 21:53:08 +1000

    init: initial version

---
+++ Matrix.Agent.Directory/Controllers/UserController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Matrix.Agent.Directory.Business.Services;
+using Matrix.Agent.Directory.Model;
+using Matrix.Framework.Api.Controllers;
+using Matrix.Framework.Api.Model;
+using Matrix.Framework.Api.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Matrix.Agent.Directory.Controllers
+{
+    [Produces("application/json")]
+    [Route("applications")]
+    public class UserController : ApiController
+    {
+        public IUserService Server { get; }
+
+        public UserController(IResponseFactory factory, IUserService server)
+            : base(factory)
+        {
+            Server = server ?? throw new ArgumentNullException(nameof(server));
+        }
+
+        [HttpGet("{Application}/users")]
+        public async Task<IActionResult> Get([FromRoute] GetUsersRequest request)
+        {
+            IActionResult result = null;
+
+            List<User> users = await Server.GetUsers(request.Application);
+
+            if (users != null)
+            {
+                result = Factory.CreateSuccessResponse(users);
+            }
+            else
+            {
+                result = Factory.CreateNoContentResponse();
+            }
+
+            return result;
+        }
+
+        [HttpGet("{Application}/users/{Id}")]
+        public async Task<IActionResult> Get([FromRoute] GetUserByIdRequest request)
+        {
+            IActionResult result = null;
+
+            var user = await Server.GetUserById(request.Id);
+
+            if (user != null)
+            {
+                result = Factory.CreateSuccessResponse(user);
+            }
+            else
+            {
+                result = Factory.CreateNoContentResponse();
+            }
+
+            return result;
+        }
+
+        [HttpPost("{Application}/users")]
+        public async Task<IActionResult> Post([FromRoute] Request meta, [FromBody] CreateUserRequest request)
+        {
+            IActionResult result = null;
+
+            var id = await Server.CreateUser(meta.Application, request.FirstName, request.LastName, request.Username, request.Password, request.Email, request.Phone);
+
+            if (id != Guid.Empty)
+            {
+                result = Factory.CreateSuccessResponse(id);
+            }
+            else
+            {
+                result = Factory.CreateNoContentResponse();
+            }
+
+            return result;
+        }
+
+        [HttpPut("{Application}/users")]
+        public async Task<IActionResult> Put([FromRoute] Request meta, [FromBody] UpdateUserRequest request)
+        {
+            IActionResult result = null;
+
+            var updated = await Server.UpdateUserProfile(request.Id, request.FirstName, request.LastName, request.Email, request.Phone);
+
+            if (updated)
+            {
+                result = Factory.CreateSuccessResponse(updated);
+            }
+            else
+            {
+                result = Factory.CreateNoContentResponse();
+            }
+
+            return result;
+        }
+
+        [HttpDelete("{Application}/users/{Id}")]
+        public async Task<IActionResult> Delete([FromRoute] DeleteUserRequest request)
+        {
+            IActionResult result = null;
+
+            var deleted = await Server.DeleteUser(request.Id);
+
+            if (deleted)
+            {
+                result = Factory.CreateSuccessResponse(deleted);
+            }
+            else
+            {
+                result = Factory.CreateNoContentResponse();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Matrix.Agent.Directory/Program.cs
@@ -0,0 +1,42 @@
+﻿using Matrix.Agent.Directory.Database.Converters;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PowerMapper;
+
+namespace Matrix.Agent.Directory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Mapper.RegisterConverter<Model.User, Database.Entities.User>(new UserConverter().ConvertToEntity);
+            Mapper.RegisterConverter<Database.Entities.User, Model.User>(new UserConverter().ConvertToModel);
+
+            var host = new WebHostBuilder();
+            host.UseKestrel();
+            host.UseContentRoot(System.IO.Directory.GetCurrentDirectory());
+
+            host.ConfigureAppConfiguration((context, configuration) =>
+            {
+                var environment = context.HostingEnvironment;
+
+                configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                configuration.AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                configuration.AddJsonFile($"/run/secrets/appsettings_shared.json", optional: true, reloadOnChange: true);
+                configuration.AddJsonFile($"/run/secrets/appsettings_directory.json", optional: true, reloadOnChange: true);
+                configuration.AddEnvironmentVariables();
+            });
+
+            host.ConfigureLogging((context, logging) =>
+            {
+                logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+            });
+
+            host.UseStartup<Startup>();
+            host.Build().Run();
+        }
+    }
+}
